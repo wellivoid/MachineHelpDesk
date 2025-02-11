0a8196e085d51318be16f506b87ec6a7a92d6eee
wellivoid <wellivoid@gmail.com>
2025-02-11 17:47:47 -0300

    Atualização de tema

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,7 +4,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
@@ -15,46 +15,68 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
-  </header>
+  </header> -->
+  <header>
+  <nav style="">
+    <div>
+      <IftaLabel style="height: 10em; width: 50em; border-color: black; border-width: 2em;">
+            <InputText id="username" v-model="value" />
+            <label for="username">Username</label>
+        </IftaLabel>
+    </div>
 
-  <RouterView />
+    <!-- <Button>Teste</Button> -->
+  </nav>
+</header>
+  <div style="background-color: blue;">
+    <RouterView />
+  </div>
+
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+/* header {
+  line-height: 2.5;
   max-height: 100vh;
-}
+} */
 
-.logo {
+/* .logo {
   display: block;
   margin: 0 auto 2rem;
+} */
+nav {
+  width: 100%;
+  height: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 0rem;
 }
 
-nav {
+
+/* nav {
   width: 100%;
   font-size: 12px;
   text-align: center;
   margin-top: 2rem;
-}
+} */
 
-nav a.router-link-exact-active {
+/* nav a.router-link-exact-active {
   color: var(--color-text);
-}
+} */
 
-nav a.router-link-exact-active:hover {
+/* nav a.router-link-exact-active:hover {
   background-color: transparent;
-}
+} */
 
-nav a {
+/* nav a {
   display: inline-block;
   padding: 0 1rem;
   border-left: 1px solid var(--color-border);
-}
+} */
 
-nav a:first-of-type {
+/* nav a:first-of-type {
   border: 0;
-}
+} */
 
 @media (min-width: 1024px) {
   header {
@@ -63,9 +85,9 @@ nav a:first-of-type {
     padding-right: calc(var(--section-gap) / 2);
   }
 
-  .logo {
+  /* .logo {
     margin: 0 2rem 0 0;
-  }
+  } */
 
   header .wrapper {
     display: flex;
@@ -73,13 +95,13 @@ nav a:first-of-type {
     flex-wrap: wrap;
   }
 
-  nav {
+  /* nav {
     text-align: left;
     margin-left: -1rem;
     font-size: 1rem;
 
     padding: 1rem 0;
     margin-top: 1rem;
-  }
+  } */
 }
 </style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,6 +2,11 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+
+import Button from "primevue/button";
+import IftaLabel from 'primevue/iftalabel';
 
 import App from './App.vue'
 import router from './router'
@@ -9,6 +14,31 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(PrimeVue);
+
 app.use(router)
+app.use(PrimeVue, {
+  // Default theme configuration
+  theme: {
+      preset: Aura,
+      options: {
+          prefix: 'p',
+          darkModeSelector: 'system',
+          cssLayer: false
+      }
+  }
+});
+
+app.component('Button', Button);
+app.component('IftaLabel', IftaLabel);
 
 app.mount('#app')
+
+
+
+
+
+
+
+
+
