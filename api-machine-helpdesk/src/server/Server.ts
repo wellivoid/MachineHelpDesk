8fafa9d7534f642da8661f68ccd3a415bdde3bd1
wellivoid <wellivoid@gmail.com>
2025-02-20 19:20:49 -0300

    Add axios e cors

--- api-machine-helpdesk/src/server/Server.ts
+++ api-machine-helpdesk/src/server/Server.ts
@@ -1,10 +1,12 @@
 import express from 'express'; 
 import { router } from './routes';
+import cors from 'cors';
 import 'dotenv/config';
 
 const server = express();
 
 server.use(express.json());
+server.use(cors());
 server.use(router);
 
 export default server;
--- machine-helpdesk-frontend/app.vue
+++ machine-helpdesk-frontend/app.vue
@@ -5,8 +5,39 @@
     <h1>{{ $t('welcome') }}</h1>
     <Button @click="setLocale('pt')" label="PtBr" />
     <Button @click="setLocale('en')" label="en" />
+    <Button @click="postData()" label="POST" />
+    <label >{{ resp }}</label>
   </div>
 </template>
-<script setup>
+
+
+<script setup="ts">
   const { setLocale } = useI18n()
+  import axios from 'axios';
+
+
+  // const create = () =>{
+  //   const httpResp = axios.post('http://localhost:3333/called',
+  //   {
+  //     title: "Deu certo",
+  //     description: "nada que deu certo no 3 ",
+  //     priority: "low",
+  //     status: "Open",
+  //     userId: 1
+  //   })
+    
+  // }
+  const resp = ref('')
+
+  const postData = async () =>{
+    const httpResp = await axios.post('http://localhost:3333/called',
+    {
+      title: "Deu certo",
+      description: "nada que deu certo no 3 ",
+      priority: "low",
+      status: "Open",
+      userId: 1
+    })
+    resp.value = JSON.stringify(httpResp.data)
+  }
 </script>
